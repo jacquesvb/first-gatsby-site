4d4d8c11d2de45759c5bf15f513b62fc80c5cb6a
Jacques Van Blokland <jacquesvb@live.com>
2022-11-29 23:14:08 -0800

    Add image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import React from "react"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage = () => {
     return (
         <Layout pageTitle="Home Page">
             <p>I'm making this by following the Gatsby Tutorial.</p>
+            <StaticImage
+                alt="Phillip Bastian age 18"
+                src="../images/Philip Bastian 18.PNG" 
+            />
         </Layout>
     )
 }
